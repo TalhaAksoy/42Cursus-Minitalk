6f5b09187aa9b779b5252e3be63b4596ce78aacc
Talha Aksoy <56833887+TalhaAksoy@users.noreply.github.com>
2022-03-01 10:37:26 +0300

    Update client.c

--- client.c
+++ client.c
@@ -27,14 +27,14 @@ void	ft_kill(int pid, char *str)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(200);
+			usleep(50);
 		}
 	}
 	i = 8;
 	while (i--)
 	{
 		kill(pid, SIGUSR2);
-		usleep(200);
+		usleep(50);
 	}
 }
 
